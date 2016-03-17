7f92f92fc40be6bb2835d9ffd806c969e0287af8
Javier Molina <javiermolinar@live.com>
2016-03-17 10:47:35 +0100

    Example of how to get access to the namespace using token authentication, examples of how to create new topics creating policy rules.

---
+++ SasAuthForServiceBus/SasAuthForServiceBus/NamespaceMgr.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SasAuthForServiceBus
+{
+    public class NamespaceMgr
+    {
+        private readonly NamespaceManager _internalNamespaceManager;
+
+
+        public NamespaceMgr(string namespaceName, string namespaceSharedAccessKey, string namespacePolicyName)
+        {
+            //First we need to build the uri
+            Uri runtimeUri = ServiceBusEnvironment.CreateServiceUri("sb", namespaceName, string.Empty);
+
+            //We need to generate the token using the shared key and the key name
+            TokenProvider token = TokenProvider.CreateSharedAccessSignatureTokenProvider(namespacePolicyName, namespaceSharedAccessKey);
+
+            //We can create a new namespace manager now
+            _internalNamespaceManager = new NamespaceManager(runtimeUri, token);
+        }
+
+     
+        //Namespace police must has manage clains in order to create, delete, update or list topics, queues or subscriptions
+        public  void CreateTopic(TopicDescription td )
+        {
+            //Just delete it if already exists. Only for testing.
+            DeleteTopic(td.Path);
+            _internalNamespaceManager.CreateTopic(td);
+         
+        }
+
+        public void DeleteTopic(string pathname)
+        {
+            _internalNamespaceManager.DeleteTopic(pathname);
+        }
+
+        public void UpdateTopic(TopicDescription td)
+        {
+            _internalNamespaceManager.UpdateTopic(td);
+        }
+        
+        //Namespace police must has manage clains in order to create, delete, update or list topics, queues or subscriptions
+        public  void CreateSubscription(string pathName, string subscriptionName)
+        {
+            if (!_internalNamespaceManager.TopicExists(pathName))
+            {
+                _internalNamespaceManager.CreateSubscription(pathName, subscriptionName);
+            }
+        }
+
+    }
+}
---
+++ SasAuthForServiceBus/SasAuthForServiceBus/SaSAuthentication.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SasAuthForServiceBus
+{
+    internal class SaSAuthentication
+    {
+        private const string NamespaceName = "";
+        private const string NamespaceSharedAccessKey = "";
+        private const string NamespacePolicyName = "NuevaPolicy";
+
+        static void Main()
+        {
+            NamespaceMgr mgrWithRights = new NamespaceMgr(NamespaceName,NamespaceSharedAccessKey,NamespacePolicyName);
+
+            TopicDescription td = new TopicDescription("Vegetables");
+            var authListen = new SharedAccessAuthorizationRule("VegetablesListenRule", new[] {AccessRights.Listen});
+            var authSend = new SharedAccessAuthorizationRule("VegetablesSendRule", new[] { AccessRights.Send });
+
+            td.Authorization.Add(authListen);
+            td.Authorization.Add(authSend);
+
+            mgrWithRights.CreateTopic(td);
+            mgrWithRights.CreateSubscription("Vegetables", "VegetablesSubscription");
+
+
+            //If policy has manage or listen claims must be possible to subscribe to a topic and start listening
+            //The policy must be created AFTER the topic is created
+            MessagingFactoryMgr listenFactory = new MessagingFactoryMgr(NamespaceName, authListen.PrimaryKey, "VegetablesListenRule");
+
+            //We can create a subscription since the policy allow to listen
+            SubscriptionClient subscription = listenFactory.GetSubscriptionClientClient(td.Path, "AllMessages");
+
+            //If policy has manage or send claims must be possible to send a messag3e to the topic
+            //The policy must be created AFTER the topic is created
+            MessagingFactoryMgr sendFactory = new MessagingFactoryMgr(NamespaceName, authSend.PrimaryKey, "VegetablesSendRule");
+
+            //We can create a new topic client since the policy allow to send
+            TopicClient topicClient = sendFactory.GetTopicClient(td.Path);
+
+
+            OnMessageOptions options = new OnMessageOptions
+            {
+                AutoComplete = false,
+                AutoRenewTimeout = TimeSpan.FromMinutes(1)
+            };
+
+            //Callback to be executed on message
+            subscription.OnMessage(message =>
+            {
+                try
+                {
+                    Console.WriteLine("We have receieved a message");
+                    Console.WriteLine($"With messageId = {message.MessageId}");
+                }
+                catch (Exception)
+                {
+                    message.Abandon();
+                }
+
+            },options);
+
+
+            Console.WriteLine("Enter M to send a message or F to finish");
+            var imput = Console.ReadLine();
+            while (imput != "f")
+            {
+                if (imput == "m")
+                {
+                    //Send a new message
+                    topicClient.Send(new BrokeredMessage());
+                }
+
+                imput = Console.ReadLine();
+            }
+        }
+    }
+}
